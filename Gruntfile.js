fb872341b0dc3fbe32a50389bbcab0916e3bbbbe
Haritz Medina <haritzmedina@gmail.com>
2016-04-22 16:20:46 +0200

    Added jasmine testing and travis

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,13 +11,25 @@ module.exports = function(grunt) {
                 src: 'src/*.js',
                 dest: 'build/<%= pkg.name %>.min.js'
             }
+        },
+        jasmine : {
+            // Your project's source files
+            src : 'src/*.js',
+            // Your Jasmine spec files
+            specs : 'specs/*spec.js',
+            // Your spec helper files
+            helpers : 'specs/helpers/*.js'
         }
+
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
+    // Register tasks.
+    grunt.loadNpmTasks('grunt-jasmine-runner');
+
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['uglify', 'jasmine']);
 
 };
--- src/core.js
+++ src/core.js
@@ -1,3 +1,8 @@
-var example = function(){
-    console.log("Example");
+var FDOMBuilder = function(selector){
+    this._rootElement = document.querySelector(selector);
+};
+
+FDOMBuilder.prototype.createElem = function(elem){
+    var child = this._root.createElement(elem);
+    this._root.appendChild(child);
 };
