09e21e35ae87140c42c2286e183a10106fc2b33f
Haritz Medina <haritzmedina@gmail.com>
2016-05-10 12:14:57 +0200

    - Finished TODO functionalities - Added documentation in jsdoc - Added jsdoc grunt task - Created tests using jasmine for the child/endchild behaviour - Minor fixes

--- test/FluentDOM.Test.js
+++ test/FluentDOM.Test.js
@@ -1,19 +1,32 @@
 describe("FDOMBuilder test", function(){
-    var builder = new FDOMBuilder();
 
-    builder.child("a")
-           .title("My personal Github")
-           .href("http://www.github.com/haritzmedina")
-           .target("_blank")
-           .id("github")
-           .child("img")
-            .src("https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-128.png")
-            .addClass("icon")
-            .css("height:128px;width:128px;") // Remember that is better to use a css file for this!
-           .endChild()
-           .endChild();
+    var builder;
 
-    it("Created an anchor called by github id, with a title and href and img as ", function() {
-        expect(true).toBe(true);
+    beforeAll(function(){
+        builder = new FDOMBuilder();
+
+        builder.child("a")
+               .title("My personal Github")
+               .href("http://www.github.com/haritzmedina")
+               .target("_blank")
+               .id("github")
+               .child("img")
+               .src("https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-128.png")
+               .addClass("icon")
+               .css("height:128px;width:128px;") // Remember that is better to use a css file for this!
+               .endChild()
+               .endChild();
+    });
+
+    it("A link to github is created with an img as a child", function() {
+        var link = document.getElementById("github");
+        var img = link.childNodes[0];
+        var name = img.tagName;
+        expect(name).toBe("IMG");
+    });
+
+    it("Check all childs are closed", function(){
+        builder._currentElement = builder._rootElement;
+
     });
 });
