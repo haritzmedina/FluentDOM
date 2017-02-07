4a153b3542de00c78e9fd396526bd44c80016ce4
Haritz Medina <haritzmedina@gmail.com>
2017-02-07 16:42:34 +0100

    Added express to start server in heroku with the example

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+let app = express();
+
+app.use('/build', express.static(__dirname + '/build'));
+app.use('/', express.static(__dirname + '/examples'));
+
+
+app.get('/', function (req, res) {
+	res.sendFile('index.html');
+});
+
+app.listen(80, function () {
+	console.log('Example app listening on port 80!');
+});
